21070346f8842a176fdab5837246399e875b99d3
Andrej Fritz <fritz.andrej@me.com>
2018-04-28 23:16:07 +0200

    updated home

--- src/components/home.js
+++ src/components/home.js
@@ -4,22 +4,7 @@ const challengeCard = require('./challengeCard')
 module.exports = function renderHome (state, emit) {
   return html`
   <div class="content-padded animated fadeIn">
-      <h1>Your scores</h1>
-      <div>
-        <small>Acceleration</small>
-        <div class="chart-bar chart-bar-red" style="width:70%;"></div>
-      </div>
-      <div>
-        <small>Braking</small>
-        <div class="chart-bar chart-bar-green" style="width:30%;"></div>
-      </div>
-      <div>
-        <small>Misc</small>
-        <div class="chart-bar chart-bar-blue" style="width:90%;"></div>
-      </div>
-      <br>
-      <h1>Running challenges</h1>
-      ${state.challenges.length > 0 ? state.challenges.map(c => challengeCard(c)) : html`<span>You have no running challenges</span>`}
+      <h1>Home</h1>
   </div>
   `
 }
