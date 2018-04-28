645f6457369c8831ba5afb21264a2a4f54a2e095
Andrej Fritz <fritz.andrej@me.com>
2018-04-28 12:55:14 +0200

    fixed auth

--- src/auth.js
+++
@@ -1,3 +0,0 @@
-module.exports = function auth (state, emitter) {
-  if (!state.user) emitter.emit('replaceState', '/login')
-}
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,13 +1,29 @@
 module.exports = function setupActions (state, emitter) {
   emitter.on('DOMContentLoaded', () => {
+    console.log(state)
     const lsUserStr = window.localStorage.getItem('InChallenge.user')
     if (lsUserStr) {
       console.log('User found in LocalStorage')
       try {
         state.user = JSON.parse(lsUserStr)
+        emitter.emit('replaceState', '/')
+        emitter.emit('render')
       } catch (e) {
         state.user = null
+        emitter.emit('replaceState', '/login')
+        emitter.emit('render')
       }
+    } else {
+      state.user = null
+      emitter.emit('replaceState', '/login')
+      emitter.emit('render')
+    }
+  })
+
+  emitter.on('navigate', () => {
+    if (!(state.route === 'login') && !state.user) {
+      emitter.emit('replaceState', '/login')
+      emitter.emit('render')
     }
   })
 
@@ -23,6 +39,7 @@ module.exports = function setupActions (state, emitter) {
   emitter.on('logout', () => {
     state.user = null
     window.localStorage.removeItem('InChallenge.user')
+    emitter.emit('replaceState', '/login')
     emitter.emit('render')
   })
 }
