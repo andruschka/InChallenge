5f7af7296bb6ccc54f1cf95322067de4ae3e32d1
tony <tonygon@gmail.com>
2018-04-29 12:37:43 +0200

    Finally 🎉 it is left aligned

--- src/components/profile.js
+++ src/components/profile.js
@@ -6,22 +6,25 @@ module.exports = function renderProfile(state, emit) {
   <div class="content-padded animated fadeIn">
       <div class="rised-card space-top">
         <div class="card-header">
-          <div class="row">
-            <div class="row left">
+          <div class="profile-info-container">
+            <div class=".img-profile">
               <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/Harry-Potter-1-.jpg" alt="" class="img-profile clip-circle">
             </div>
-            <div class="column right">
+            <div class="profile-info">
               <ul >
                 <li class="table-view-cell">Joe Smith</li>
                 <li class="table-view-cell">+49 123 12345678</li>
-                <li class="table-view-cell">joe.smith@mail.com</li>
+                <li class="table-view-cell">joe@mail.com</li>
                 <li class="table-view-cell">Policy Nr. #123456789</li>
-                <li class="table-view-cell">Passwort</li>
+                
                 <li class="table-view-cell"><span class="badge badge-primary">Edit</span></li>
+                <li class="table-view-cell"></li>
               </ul>    
             </div>
           </div>
         </div>
+
+
         
         <div class="space-top score-stats">
           <div class="stat text-center">
